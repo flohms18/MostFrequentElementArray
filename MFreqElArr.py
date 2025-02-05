a3292f96f1f5f01ac0cfd559bc3e70573211972a
flotr18 <trouilletflorent@outlook.com>
2025-02-05 10:09:54 +0100

    Max Function added

--- MFreqElArr.py
+++ MFreqElArr.py
@@ -1,5 +1,5 @@
 def MostFrequentElementinArray():
-    array = [1, 2, 8, 3, 2, 2, 2, 5, 1];
-
-
+    MyArray = [1, 2, 8, 3, 2, 2, 2, 5, 1]
+    print(max(MyArray, key = MyArray.count))
+                
 MostFrequentElementinArray()
